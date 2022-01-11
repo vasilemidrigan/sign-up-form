a6d5b3e0e5b37989f63cb9bf4b16dc9cc49c26db
Vasile Midrigan <111rigan111@gmail.com>
2022-01-11 23:49:25 +0000

    form validation implemented

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 "use strict";
-// Variables
+// Constants
 const form = document.getElementById("form");
 const firstName = document.getElementById("first_name");
 const lastName = document.getElementById("last_name");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const submit = document.getElementById("submit");
+const inputErrorState = document.querySelectorAll(".input_error_state");
+
+// =====================================================================
+
+// Password regexp constant:
 
 // The valid password should contain at least:
 // - one capital letter
@@ -13,110 +18,126 @@ const submit = document.getElementById("submit");
 // - at least 8 characters
 const validPassword = new RegExp("^(?=.*[A-Z])(?=.*[0-9])(?=.{8,})");
 
-// Display error function
-function displayError() {
-  // First Name Error
-  if (firstName.value === "") {
-    console.log("firstName empty");
-    firstName.parentElement.children[1].classList.remove("hidden");
-    firstName.parentElement.children[2].classList.remove("hidden");
-    firstName.parentElement.children[2].innerHTML =
-      "Please enter your first name.";
-  }
-  // Last Name Error
-  if (lastName.value === "") {
-    console.log("firstName empty");
-    lastName.parentElement.children[1].classList.remove("hidden");
-    lastName.parentElement.children[2].classList.remove("hidden");
-    lastName.parentElement.children[2].innerHTML =
-      "Please enter your last name.";
+// ========================  Display error function
+function displayError(inputField) {
+  // First Name and Last Name Error
+  if (inputField === firstName || inputField === lastName) {
+    inputField.classList.add("input_error_state");
+    inputField.parentElement.children[1].classList.remove("hidden");
+    inputField.parentElement.children[2].classList.remove("hidden");
+    if (inputField === firstName) {
+      inputField.parentElement.children[2].innerHTML =
+        "Please enter your first name.";
+    } else {
+      inputField.parentElement.children[2].innerHTML =
+        "Please enter your last name.";
+    }
   }
   // Email Error
-  if (email.validity.valueMissing) {
-    console.log("email valuemissing error");
-    email.parentElement.children[2].classList.remove("hidden");
-    email.parentElement.children[1].classList.remove("hidden");
-    email.parentElement.children[2].innerHTML =
-      "Please enter the email address.";
-  } else if (email.validity.typeMismatch) {
-    console.log("email type mismatch error");
-    email.parentElement.children[2].classList.remove("hidden");
-    email.parentElement.children[1].classList.remove("hidden");
-    email.parentElement.children[2].textContent =
-      "Please provide a valid email address.";
+  if (inputField === email) {
+    if (email.validity.valueMissing) {
+      email.classList.add("input_error_state");
+      email.parentElement.children[1].classList.remove("hidden");
+      email.parentElement.children[2].classList.remove("hidden");
+      email.parentElement.children[2].innerHTML =
+        "Please enter the email address.";
+    } else if (email.validity.typeMismatch) {
+      email.classList.add("input_error_state");
+      email.parentElement.children[1].classList.remove("hidden");
+      email.parentElement.children[2].classList.remove("hidden");
+      email.parentElement.children[2].textContent =
+        "Please provide a valid email address.";
+    }
   }
   // Password Error
-  if (!validPassword.test(password.value)) {
-    console.log("PASS ERROR!!!");
-    password.parentElement.children[2].classList.remove("hidden");
-    password.parentElement.children[1].classList.remove("hidden");
-    password.parentElement.children[2].innerHTML =
-      "Please enter a password with minimum 8 characters at least one number and one letter..";
+  if (inputField === password) {
+    if (!validPassword.test(password.value)) {
+      password.classList.add("input_error_state");
+      password.parentElement.children[1].classList.remove("hidden");
+      password.parentElement.children[2].classList.remove("hidden");
+      password.parentElement.children[2].innerHTML =
+        "Min 8 characters, one nr. and one letter.";
+    }
   }
 }
 // First Name Input Event Listener
-firstName.addEventListener("input", function () {
-  if (firstName) {
-    console.log("First Name Not Empty!");
-    firstName.parentElement.children[1].classList.add("hidden");
-    firstName.parentElement.children[2].classList.add("hidden");
-  } else {
-    displayError();
-    console.log("First Name Empty!");
-  }
-});
+firstName.addEventListener(
+  "input",
+  function () {
+    if (firstName.value !== "") {
+      firstName.classList.remove("input_error_state");
+      firstName.parentElement.children[1].classList.add("hidden");
+      firstName.parentElement.children[2].classList.add("hidden");
+    } else {
+      displayError(firstName);
+      console.log("First Name Empty!");
+    }
+  },
+  true
+);
 // Last Name Input Event Listener
-lastName.addEventListener("input", function () {
-  if (lastName) {
-    console.log("Last Name Not Empty!");
-    lastName.parentElement.children[1].classList.add("hidden");
-    lastName.parentElement.children[2].classList.add("hidden");
-  } else {
-    displayError();
-    console.log("Last Name Empty!");
-  }
-});
+lastName.addEventListener(
+  "input",
+  function () {
+    if (lastName.value !== "") {
+      lastName.classList.remove("input_error_state");
+      lastName.parentElement.children[1].classList.add("hidden");
+      lastName.parentElement.children[2].classList.add("hidden");
+    } else {
+      displayError(lastName);
+      console.log("Last Name Empty!");
+    }
+  },
+  true
+);
 // Email Input Event Listener
-email.addEventListener("input", function () {
-  if (email.validity.valid) {
-    email.parentElement.children[2].classList.add("hidden");
-    email.parentElement.children[1].classList.add("hidden");
-  } else {
-    displayError();
-  }
-});
+email.addEventListener(
+  "input",
+  function () {
+    if (email.validity.valid) {
+      email.parentElement.children[2].classList.add("hidden");
+      email.parentElement.children[1].classList.add("hidden");
+      email.classList.remove("input_error_state");
+    } else {
+      displayError(email);
+    }
+  },
+  true
+);
 // Password Input Event Listener
-console.log(password.value);
-password.addEventListener("input", function () {
-  if (validPassword.test(password.value)) {
-    console.log("password matches");
-    password.parentElement.children[1].classList.add("hidden");
-    password.parentElement.children[2].classList.add("hidden");
-  } else {
-    console.log("password NOT  match");
-    displayError();
-  }
-});
+password.addEventListener(
+  "input",
+  function () {
+    if (validPassword.test(password.value)) {
+      password.parentElement.children[1].classList.add("hidden");
+      password.parentElement.children[2].classList.add("hidden");
+      password.classList.remove("input_error_state");
+    } else {
+      displayError(password);
+    }
+  },
+  true
+);
 // Submit Input Event Listener
 form.addEventListener("submit", function (e) {
   // First Name Submit
   if (firstName.value === "") {
-    displayError();
+    displayError(firstName);
     e.preventDefault();
   }
   // Last Name Submit
   if (lastName.value === "") {
-    displayError();
+    displayError(lastName);
     e.preventDefault();
   }
   // Email Submit
   if (!email.validity.valid) {
-    displayError();
+    displayError(email);
     e.preventDefault();
   }
   // Password Submit
   if (!validPassword.test(password.value)) {
-    displayError();
+    displayError(password);
     e.preventDefault();
   }
 });
